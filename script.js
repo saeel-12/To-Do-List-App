b133bdc61fb681b99398a10e5ddd0ea108144127
Saeel Shinde <saeelshinde12@gmail.com>
2025-08-21 11:37:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,14 +21,20 @@ function addTask() {
 
 listContainer.addEventListener("click" , function(e){
     if (e.target.tagName==="LI") {
-        e.target.classList.toggle("checked");        
+        e.target.classList.toggle("checked");   
+        saveData();     
     }
     else if(e.target.tagName==="SPAN"){
 
-        e.target.parentElement.remove();        
+        e.target.parentElement.remove();
+        saveData();        
     }
 },false);
 
 function saveData(){
     localStorage.setItem("data",listContainer.innerHTML);
 }
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
